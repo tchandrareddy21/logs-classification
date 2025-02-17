6c41431b116ace2ca77b6e8ed3fbafa316570646
Tiyyagura Chandra Reddy <tchandrareddy21@gmail.com>
2025-02-17 06:33:28 +0530

    backend fastapi added

---
+++ fastapi_backend.py
@@ -0,0 +1,45 @@
+import pandas as pd
+from fastapi import FastAPI, UploadFile, HTTPException
+from fastapi.responses import FileResponse
+from logger import logging
+from exception import  LoggerException
+from pipeline.classifier import classify
+import io
+
+app = FastAPI()
+
+
+@app.post("/classify/")
+async def classify_logs(file: UploadFile):
+    if not file.filename.endswith('.csv'):
+        logging.error(f"File type not supported. File must be .csv {file}")
+        raise HTTPException(status_code=400, detail="File must be a CSV.")
+
+    try:
+        # Read the uploaded CSV
+        # df = pd.read_csv(file.file)
+        logging.info("Reading file")
+        # df = pd.read_csv(io.StringIO(file.file.read().decode("utf-8")))
+        content = await file.read()
+        df = pd.read_csv(io.StringIO(content.decode("utf-8")))
+        print(df)
+        if "source" not in df.columns or "log_message" not in df.columns:
+            logging.error("Missing source and log_message columns in file.")
+            raise HTTPException(status_code=400, detail="CSV must contain 'source' and 'log_message' columns.")
+
+        # Perform classification
+        logging.info("Calling classify function.")
+        df["target_label"] = classify(list(zip(df["source"], df["log_message"])))
+        logging.info("Classified successfully.")
+
+        print("Dataframe:", df.to_dict())
+
+        # Save the modified file
+        output_file = "classified_logs.csv"
+        df.to_csv(output_file, index=False)
+        print("File saved to output.csv")
+        return FileResponse(output_file, media_type='text/csv')
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    finally:
+        file.file.close()
